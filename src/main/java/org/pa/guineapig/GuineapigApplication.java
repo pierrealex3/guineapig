64d9ac1285b8da95be4a2d9766fa8b22fe1a0797
PA Lemire <pierrealex3@hotmail.com>
2021-04-14 11:40:33 -0400

    First commit

---
+++ src/main/java/org/pa/guineapig/GuineapigApplication.java
@@ -0,0 +1,13 @@
+package org.pa.guineapig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuineapigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuineapigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/pa/guineapig/GuineapigApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pa.guineapig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuineapigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
